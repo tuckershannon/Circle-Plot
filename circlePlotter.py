f7ca2f7d86c26a608a0cf6e9461da146736dd34b
tuckershannon <tuckerleeshannon@gmail.com>
2021-01-24 21:37:09 -0800

    First commit of circle plot experiment

--- circlePlotter.py
+++ circlePlotter.py
@@ -20,10 +20,10 @@ class stepperMotor:
     def takeStep(self, direction):
         # time.sleep(1/self.speed)
         if direction == 1:
-            #print("Motor " + self.name + " step forward")
+            # print("Motor " + self.name + " step forward")
             self.stepCount += 1
         elif direction == 2:
-            #print("Motor " + self.name + " step backward")
+            # print("Motor " + self.name + " step backward")
             self.stepCount -= 1
         else:
             print("Incorrect direction. Please input direction 1 or 2")
@@ -78,8 +78,6 @@ class circlePlotter:
         plt.plot(xPoints, yPoints)
         plt.show()
 
-
-
     def keyboardcontrol(self):
         print("finish this?")
 
@@ -92,7 +90,7 @@ class circlePlotter:
         self.showPathTaken()
 
     def moveToPoint(self, point):
-
+        startPoint = self.currentPos
         while not self.closeEnough(point):
             # there are 4 possible movements we can do
             # 1. Move rMotor 1 step forward
@@ -110,7 +108,16 @@ class circlePlotter:
             self.rMotorStepMovementUpdate()
             self.thetaMotorStepMovementUpdate()
 
-            # Check distance change if we move rMotor 1 step forward
+            p1x = self.currentPos[0] + self.rMotor.mmPerStepX
+            p1y = self.currentPos[1] + self.rMotor.mmPerStepY
+            p2x = self.currentPos[0] - self.rMotor.mmPerStepX
+            p2y = self.currentPos[1] - self.rMotor.mmPerStepY
+            p3x = self.currentPos[0] + self.thetaMotor.mmPerStepX
+            p3y = self.currentPos[1] + self.thetaMotor.mmPerStepY
+            p4x = self.currentPos[0] - self.thetaMotor.mmPerStepX
+            p4y = self.currentPos[1] - self.thetaMotor.mmPerStepY
+
+
             dx1 = point[0] - (self.currentPos[0] + self.rMotor.mmPerStepX)
             dy1 = point[1] - (self.currentPos[1] + self.rMotor.mmPerStepY)
             # Check distance change if we move rMotor 1 step backward
@@ -124,38 +131,63 @@ class circlePlotter:
             dy4 = point[1] - (self.currentPos[1] - self.thetaMotor.mmPerStepY)
 
             d = math.sqrt(dx ** 2 + dy ** 2)
-            d1 = math.sqrt(dx1 ** 2 + dy1 ** 2)
-            d2 = math.sqrt(dx2 ** 2 + dy2 ** 2)
-            d3 = math.sqrt(dx3 ** 2 + dy3 ** 2)
-            d4 = math.sqrt(dx4 ** 2 + dy4 ** 2)
+            d1t = math.sqrt(dx1 ** 2 + dy1 ** 2)
+            d2t = math.sqrt(dx2 ** 2 + dy2 ** 2)
+            d3t = math.sqrt(dx3 ** 2 + dy3 ** 2)
+            d4t = math.sqrt(dx4 ** 2 + dy4 ** 2)
+
+            d1 = self.pointLineDist(point[0],point[1],startPoint[0],startPoint[1],p1x,p1y)
+            d2 = self.pointLineDist(point[0], point[1],startPoint[0],startPoint[1], p2x, p2y)
+            d3 = self.pointLineDist(point[0], point[1],startPoint[0],startPoint[1], p3x, p3y)
+            d4 = self.pointLineDist(point[0], point[1],startPoint[0],startPoint[1], p4x, p4y)
+
+            if d < d1t:
+                d1 = 0
+            if d < d2t:
+                d2 = 0
+            if d < d3t:
+                d3 = 0
+            if d < d4t:
+                d4 = 0
+
+
+            if d <= d1 or d <= d2 or d <= d3 or d <= d4:
+                d1 = d1t
+                d2 = d2t
+                d3 = d3t
+                d4 = d4t
 
             # check to see if all actions will result in being further from goal
-            if d <= d1 and d <= d2 and d <= d3 and d <= d4:
+            if (d <= d1 or d1 == 0) and (d <= d2 or d2 == 0) and (d <= d3 or d3 ==0) and (d <= d4 or d4 ==0):
                 # we are as close as possible, break out of loop
                 break
-            # now take the action with the shortest resultant distance
-            if d1 <= d2 and d1 <= d3 and d1 <= d4:
+
+            distanceList = [d1,d2,d3,d4]
+            minDistance = min([x for x in distanceList if x != 0])
+            minDistance = distanceList.index(minDistance)
+
+            if minDistance == 0:
                 self.takeStep(1, 1)
-            elif d2 <= d1 and d2 <= d3 and d2 <= d4:
+            elif minDistance == 1:
                 self.takeStep(1, 2)
-            elif d3 <= d1 and d3 <= d2 and d3 <= d4:
+            elif minDistance == 2:
                 self.takeStep(2, 1)
-            elif d4 <= d1 and d4 <= d2 and d4 <= d3:
+            else:
                 self.takeStep(2, 2)
 
             print(self.currentPos)
 
     def closeEnough(self, point):
-        if abs(self.currentPos[0] - point[0]) < self.rMotor.mmPerStepX:
-            if abs(self.currentPos[1] - point[1]) < self.rMotor.mmPerStepY:
-                if abs(self.currentPos[0] - point[0]) < self.thetaMotor.mmPerStepX:
-                    if abs(self.currentPos[1] - point[1]) < self.thetaMotor.mmPerStepY:
+        if abs(self.currentPos[0] - point[0]) < abs(self.rMotor.mmPerStepX*2):
+            if abs(self.currentPos[1] - point[1]) < abs(self.rMotor.mmPerStepY*2):
+                if abs(self.currentPos[0] - point[0]) < abs(self.thetaMotor.mmPerStepX*2):
+                    if abs(self.currentPos[1] - point[1]) < abs(self.thetaMotor.mmPerStepY*2):
                         return True
         return False
 
     def rMotorStepMovementUpdate(self):
-        extensionPerStep = ((
-                                    1.0 / self.rMotor.stepsPerRev) / self.rMotor.gearRatio) * self.rMotor.gearDiameter * math.pi
+        extensionPerStep = (1.0 / self.rMotor.stepsPerRev) * self.rMotor.gearDiameter * math.pi
+
 
         self.rMotor.mmPerStepX = extensionPerStep * math.cos(self.currentTheta)
         self.rMotor.mmPerStepY = extensionPerStep * math.sin(self.currentTheta)
@@ -177,12 +209,41 @@ class circlePlotter:
         self.updateCurrentPos()
 
     def updateCurrentPos(self):
-        currentX = self.currentR * math.cos(self.currentTheta)
-        currentY = self.currentR * math.sin(self.currentTheta)
+        currentX = self.origin[0] + self.currentR * math.cos(self.currentTheta)
+        currentY = self.origin[1] + self.currentR * math.sin(self.currentTheta)
 
         self.currentPos = [currentX, currentY]
 
     def setOrigin(self):
-        #self.currentPos = self.traj[0]
-        self.currentPos = [0.0,0.0]
+        # self.currentPos = self.traj[0]
+        self.currentPos = self.traj[0]
         self.origin = self.currentPos
+
+    def pointLineDist(self, x1, y1, x2, y2, x3, y3):  # x3,y3 is the point
+        px = x2 - x1
+        py = y2 - y1
+
+        norm = px * px + py * py
+
+        u = ((x3 - x1) * px + (y3 - y1) * py) / float(norm)
+
+        if u > 1:
+            u = 1
+        elif u < 0:
+            u = 0
+
+        x = x1 + u * px
+        y = y1 + u * py
+
+        dx = x - x3
+        dy = y - y3
+
+        # Note: If the actual distance does not matter,
+        # if you only want to compare what this function
+        # returns to other results of this function, you
+        # can just return the squared distance instead
+        # (i.e. remove the sqrt) to gain a little performance
+
+        dist = (dx * dx + dy * dy) ** .5
+
+        return dist
